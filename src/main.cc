a6256f75b4b7e2d7edb0e26dd45b3e1842f9f2b9
Samuelmm15 <alu0101359526@ull.edu.es>
2022-12-19 23:19:46 +0000

    Advanced - at the implementation of the code

--- src/main.cc
+++ src/main.cc
@@ -21,36 +21,49 @@ int main(int argc, char const *argv[]) {
   std::cout << "<< BIENVENIDO AL PROGRAMA DE CAMBIO DE MONEDAS >>" << std::endl;
   double amount;
   if (argc == 2) {
+    std::string option = argv[1];
+    if (option == "-h" || option == "--help") {
+      std::cout << "AYUDA >>> Para la correcta ejecución del programa se debe de hacer uso de: " << std::endl;
+      std::cout << "$ ./coinsExchange [-b | -o]? [amount]" << std::endl;
+      std::cout << "Donde: " << std::endl;
+      std::cout << "-b: Es el modo de ejecución del programa, el cual hace uso de billetes en vez de monedas para poder calcular la cantidad a cambiar." << std::endl;
+      std::cout << "-o: Es el modo de ejecución del programa, ." << std::endl;
+      return 0;
+    } else {
+      std::cout << "ERROR: El tipo de parámetro introducido no es correcto." << std::endl;
+      std::cout << "Para más información haga uso de la opción --h o -help" << std::endl;
+      return 0; 
+    }
     amount = std::stod(argv[1]);
     coinsExchange coinsExchangeObject;
     coinsExchangeObject.setAmount(amount);
     coinsExchangeObject.SolutionFunction();
+    return 0;
   } else if (argc == 1) {
     std::cout << "Introduzca el valor objetivo, el cual quiere comprobar el número total de monedas: ";
     std::cin >> amount;
     coinsExchange coinsExchangeObject;
     coinsExchangeObject.setAmount(amount);
     coinsExchangeObject.SolutionFunction();
+    return 0;
   } else if (argc == 3) {
-    if (argv[1] == '-b') {
+    std::string option = argv[1];
+    if (option == "-b") {
       amount = std::stod(argv[2]);
       coinsExchange coinsExchangeObject;
       coinsExchangeObject.setAmount(amount);
       coinsExchangeObject.SolutionFunction();
-    } else if (argv[1] == '-o') {
+      return 0;
+    } else if (option == "-o") {
       amount = std::stod(argv[2]);
       coinsExchange coinsExchangeObject;
       coinsExchangeObject.setAmount(amount);
       coinsExchangeObject.SolutionFunction();
-    } else if (argv[1] == '-h' || argv[1] == '-help') {
-      std::cout << "AYUDA >>> Para la correcta ejecución del programa se debe de hacer uso de: " << std::endl;
-      std::cout << "$ ./coinsExchange [-b | -o]? [amount]" << std::endl;
-      std::cout << "Donde: " << std::endl;
-      std::cout << "-b: Es el modo de ejecución del programa, el cual hace uso de billetes en vez de monedas para poder calcular la cantidad a cambiar." << std::endl;
-      std::cout << "-o: Es el modo de ejecución del programa, ." << std::endl;
+      return 0;
     } else {
       std::cout << "ERROR: El tipo de parámetro introducido no es correcto." << std::endl;
       std::cout << "Para más información haga uso de la opción --h o -help" << std::endl;
+      return 0;
     }
   }
   return 0;
